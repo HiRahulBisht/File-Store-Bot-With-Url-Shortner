fe68f3aa27a5d5cc92e3c38c6ab669117d8ec519
HiRahulBisht <60350249+HiRahulBisht@users.noreply.github.com>
2023-11-30 21:22:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,14 +1,14 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+  API_ID = int(os.environ.get("API_ID", "1615127"))
+  API_HASH = os.environ.get("API_HASH", "566474478c929ad4bd441c0bd269531c")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6318429756:AAHKYzltAqO9K2pjG2739liar-4k_eBGOmU")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Shorturlsxyzbot")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "shorturls.xyz")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "e97832eea584df62e8f922789a77c5c3225f45c3)
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "712573408"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
